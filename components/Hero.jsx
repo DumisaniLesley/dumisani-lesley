4f3778e39651205e2bf1a6e9abb84fef4e53a356
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2024-03-09 17:26:32 +0200

    Update Hero component styling

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,19 +9,19 @@ const titles = [
 const Hero = () => {
   return (
     <div className="lg:max-w-[1200px] mx-auto px-4 h-screen">
-      <div className="flex flex-col items-start justify-center h-3/4 px-4">
+      <div className="flex flex-col items-start justify-center h-3/4 md:h-full px-4">
         <div>
-          <span>I am a</span>
+          <span className="md:text-2xl">I am a</span>
           <div className="">
             {titles.map((title, index) => (
-              <h1 key={index} className="text-[23px] font-bold uppercase">
+              <h1 key={index} className="text-[23px] md:text-5xl lg:text-6xl font-bold uppercase md:my-2">
                 {title}
               </h1>
             ))}
           </div>
         </div>
-        <div className="mt-2">
-          <button className="py-2 px-4 bg-blue-700 rounded-md">View Projects</button>
+        <div className="mt-2 md:mt-4">
+          <button className="py-2 px-4 md:py-4 md:px-8 md:uppercase font-bold text-md md:text-xl bg-blue-700 rounded-md">View Projects</button>
         </div>
       </div>
     </div>
