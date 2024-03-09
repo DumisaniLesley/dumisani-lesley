d0c466c950602f1d826d5984f67efef07ab90737
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2024-03-09 17:20:26 +0200

    Refactor Hero component to include dynamic titles and a button

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
+const titles = [
+  "Frontend developer,",
+  "Data analyst,",
+  "Systems analyst,",
+  "IT Specialist",
+];
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="lg:max-w-[1200px] mx-auto px-4 h-screen">
+      <div className="flex flex-col items-start justify-center h-3/4 px-4">
+        <div>
+          <span>I am a</span>
+          <div className="">
+            {titles.map((title, index) => (
+              <h1 key={index} className="text-[23px] font-bold uppercase">
+                {title}
+              </h1>
+            ))}
+          </div>
+        </div>
+        <div className="mt-2">
+          <button className="py-2 px-4 bg-blue-700 rounded-md">View Projects</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
